ea594256d58b8796b6538f34d03badf01bc0d304
bachnguyenTE <nvbach2000vn@gmail.com>
2020-05-14 19:57:11 -0500

    feature/state-mgmt: added global auth stream provider

--- lib/locator.dart
+++ lib/locator.dart
@@ -1,9 +1,10 @@
+import 'package:comet_events/core/services/auth.dart';
 import 'package:get_it/get_it.dart';
 
 GetIt locator = GetIt.instance;
 
 setupLocator() {
   // lazy singletons (services)
-  
+  locator.registerLazySingleton(() => Auth());
   // factories (view models)
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
+import 'package:comet_events/locator.dart';
 import 'package:comet_events/ui/screens/screens.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:comet_events/core/services/services.dart';
 
 void main() {
-
+  setupLocator();
   runApp(CometEvents());
 }
 
@@ -10,13 +14,17 @@ class CometEvents extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-
-        primarySwatch: Colors.purple,
+    return MultiProvider(
+      providers: [
+        StreamProvider<FirebaseUser>.value(value: locator<Auth>().user),
+      ],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.purple,
+        ),
+        home: HomeScreen(title: 'Flutter Demo Home Page'),
       ),
-      home: HomeScreen(title: 'Flutter Demo Home Page'),
     );
   }
 }
