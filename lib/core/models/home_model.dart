550f298d900347f91b30d6b1d12859322e1025f2
Jafar <orangecup6@gmail.com>
2020-06-02 05:45:33 -0500

    feature/home-screen: tweaked the home screen a bit. added home model. added new libraries

--- lib/core/models/home_model.dart
+++ lib/core/models/home_model.dart
@@ -1,10 +1,21 @@
 import 'package:comet_events/core/models/base_model.dart';
 import 'package:comet_events/core/services/services.dart';
+import 'package:comet_events/ui/widgets/event_tile.dart';
 import 'package:comet_events/utils/locator.dart';
 
 class HomeModel extends BaseModel {
 
+  // * ----- Services -----
   AuthService _auth = locator<AuthService>();
+
+  // * ----- Variables -----
+  List<EventTile> _events = [
+    EventTile(scale: 1.2, imageURL: 'https://picsum.photos/200',
+     title: 'Fwood Pwarty', date: '11:30 PM', category: 'Food', tags: ['Fun', 'Cool', 'Fresh'], description: 'Free buffet for all'),
+    EventTile(scale: 1.2, imageURL: 'https://picsum.photos/200', title: "Neha's the best", date: 'Everyday', category: 'Relegious', tags: ['True', 'Words', 'to' ,'Live', 'By'], description: 'Rare chance to hangout wiht the best person on earth!'),
+  ];
+  List<EventTile> get events => _events;
+
 
   int _count = 0;
   int get count => _count;
--- lib/ui/screens/home_screen.dart
+++ lib/ui/screens/home_screen.dart
@@ -1,16 +1,13 @@
-import 'dart:async';
-
 import 'package:comet_events/core/models/home_model.dart';
-import 'package:comet_events/core/objects/objects.dart';
 import 'package:comet_events/ui/theme/theme.dart';
-import 'package:comet_events/ui/widgets/event_tile.dart';
 import 'package:comet_events/ui/widgets/user_view_model_builder.dart';
 import 'package:comet_events/utils/locator.dart';
+import 'package:flappy_search_bar/flappy_search_bar.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:search_map_place/search_map_place.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart' show rootBundle;
 import 'package:carousel_slider/carousel_slider.dart';
+import 'dart:async';
 
 class HomeScreen extends StatefulWidget {
   HomeScreen({Key key,}) : super(key: key);
@@ -22,138 +19,122 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
 
   bool _state = false;
-  List<EventTile> events = [
-    EventTile(imageURL: 'https://picsum.photos/200',
-     title: 'Fwood Pwarty', date: 'January 9, 1876', category: 'Food', tags: ['Fun', 'Cool', 'Fresh'], description: 'Free buffet for all'),
-    EventTile(imageURL: 'https://picsum.photos/200', title: "Neha's the best", date: 'Everyday', category: 'Relegious', tags: ['True', 'Words', 'to' ,'Live', 'By'], description: 'Rare chance to hangout wiht the best person on earth!'),
-  ];
-
+  
   @override
   Widget build(BuildContext context) {
-    return UserViewModelBuilder<HomeModel>.reactive(
+
+    CometThemeData _appTheme = locator<CometThemeManager>().theme;
+
+    return Scaffold(
+      backgroundColor: _appTheme.secondaryMono,
+      body: UserViewModelBuilder<HomeModel>.reactive(
         userViewModelBuilder: () => HomeModel(),
-        builder: (context, model, user, child) => Scaffold(
-          body: Center(
-            // Here we take the value from the HomeScreen object that was created by
-            // the App.build method, and use it to set our appbar title.
-            child: Stack(
+        builder: (context, model, user, child) => Stack(
+          children: <Widget>[
+            Map(),
+            Column(
               children: <Widget>[
-                Map(),
-                Column(
-                  children: <Widget>[
-                    topAppBar(),
-                    Row(
-                      children: <Widget>[
-                        //searchBar(),
-                        Spacer(),
-                        filterButton()
-                      ],
-                    ),
-                    Spacer(),
-                    eventCarousel(),
-                    bottomAppBar()
-                  ],
+                _topAppBar(),
+                SizedBox(height: 7),
+                Expanded(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: <Widget>[
+                      _searchWidget(),
+                    ],
+                  )
                 ),
+                _eventCarousel(model),
+                SizedBox(height: 35),
+                _bottomAppBar(),
+                SafeArea(child: Container(), top: false)
               ],
-            )
-          ),
-          backgroundColor: locator<CometThemeManager>().theme.secondaryMono,
+            ),
+          ],
+        ),
       ),
     );
   }
 
-  Widget topAppBar(){
+  Widget _topAppBar(){
     return Container(
-      height: MediaQuery.of(context).size.height*0.12,
-      padding: EdgeInsets.fromLTRB(15, 40, 30, 5),
+      // height: MediaQuery.of(context).size.height*0.12,
+      padding: EdgeInsets.symmetric(horizontal: 23, vertical: 15),
       decoration: BoxDecoration(
         shape: BoxShape.rectangle,
         borderRadius: BorderRadius.only(
-          bottomLeft: Radius.circular(25.0),
-          bottomRight: Radius.circular(25.0)
+          bottomLeft: Radius.circular(30.0),
+          bottomRight: Radius.circular(30.0)
         ),
         color: locator<CometThemeManager>().theme.mainMono
       ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: <Widget>[
-          IconButton(
-            icon: Icon(Icons.settings, size: 30),
-            color: locator<CometThemeManager>().theme.mainColor,
-            //go to settings
-            onPressed: () {}
-          ),
-          Text(
-            'WELCOME, YOLANDA',
-            style: TextStyle(
-              fontFamily: "Lexend Deca",
-              fontSize: 18,
-            )
-          ),
-          FlutterLogo( size: 30 )
-        ],
+      child: SafeArea(
+        bottom: false,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Icon(
+              Icons.settings, 
+              size: 30, 
+              color: locator<CometThemeManager>().theme.mainColor,
+            ),
+            Text(
+              'WELCOME, YOLANDA',
+              style: TextStyle(
+                fontFamily: "Lexend Deca",
+                fontSize: 18,
+              )
+            ),
+            FlutterLogo( size: 30 )
+          ],
+        ),
       )
     );
   }
   
-  Widget bottomAppBar(){
+  Widget _bottomAppBar(){
     return Stack(
       overflow: Overflow.visible,
       alignment: Alignment.topCenter,
       children: <Widget>[
         Container(
-          height: MediaQuery.of(context).size.height*0.10,
+          // height: MediaQuery.of(context).size.height*0.10,
           decoration: BoxDecoration(
             color: locator<CometThemeManager>().theme.mainMono,
-            borderRadius: BorderRadius.circular(20.0),
+            borderRadius: BorderRadius.circular(25.0),
           ),
-          margin: EdgeInsets.fromLTRB(5, 0, 5, 5),
-          padding: EdgeInsets.fromLTRB(60, 5, 60, 5),
+          margin: EdgeInsets.symmetric(horizontal: 6),
+          padding: EdgeInsets.only(bottom: 10, top: 5),
           child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.end,
             children: <Widget>[
-              GestureDetector(
-                onTap: (){},
-                child: Column(
-                  children: <Widget>[
-                    Stack(
+              Expanded(
+                child: Container(
+                  // alignment: Alignment.center,
+                  child: GestureDetector(
+                    onTap: (){},
+                    child: Column(
                       children: <Widget>[
-                        Positioned(
-                          top: 2.0,
-                          left: 2.0,
-                          child: Icon(
-                            Icons.people,
-                            size: 50,
-                            color: Colors.black26,
-                          ),
-                        ),
-                        Icon(Icons.people, size: 50,color: locator<CometThemeManager>().theme.mainColor )
+                        Icon(Icons.people, size: 50,color: locator<CometThemeManager>().theme.mainColor ),
+                        Text('Fwends', style: TextStyle(color: locator<CometThemeManager>().theme.mainColor)),
                       ],
                     ),
-                    Text('Fwends', style: TextStyle(color: locator<CometThemeManager>().theme.mainColor)),
-                  ],
+                  ),
                 ),
               ),
-              GestureDetector(
-                onTap: (){},
-                child: Column(
-                  children: <Widget>[
-                    Stack(
+              SizedBox(width: 50),
+              Expanded(
+                child: Container(
+                  child: GestureDetector(
+                    onTap: (){},
+                    child: Column(
                       children: <Widget>[
-                        Positioned(
-                          top: 2.0,
-                          left: 2.0,
-                          child: Icon(
-                            Icons.message,
-                            size: 50,
-                            color: Colors.black26,
-                          ),
-                        ),
-                        Icon(Icons.message, size: 50,color: locator<CometThemeManager>().theme.mainColor )
+                        Icon(Icons.message, size: 44,color: locator<CometThemeManager>().theme.mainColor ),
+                        Text('Mwessages', style: TextStyle(color: locator<CometThemeManager>().theme.mainColor)),
                       ],
                     ),
-                    Text('Mwessages', style: TextStyle(color: locator<CometThemeManager>().theme.mainColor)),
-                  ],
+                  ),
                 ),
               )
             ],
@@ -165,11 +146,18 @@ class _HomeScreenState extends State<HomeScreen> {
             decoration: BoxDecoration(
               shape: BoxShape.circle,
               gradient: RadialGradient(
-                colors: [locator<CometThemeManager>().theme.mainColor, CometThemeManager.mainColorDark]
+                center: Alignment(-0.5,-0.5),
+                // center: Alignment.center,
+                radius: 1,
+                colors: [
+                  CometThemeManager.lighten(locator<CometThemeManager>().theme.mainColor, 0.1), 
+                  locator<CometThemeManager>().theme.mainColor, 
+                  CometThemeManager.darken(locator<CometThemeManager>().theme.mainColor, 0.35)
+                ]
               ),
               boxShadow: [
                 BoxShadow(
-                  color: Colors.black26,
+                  color: Colors.black12,
                   spreadRadius: 5,
                   blurRadius: 7,
                   offset: Offset(0, 3)
@@ -191,43 +179,58 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 
-  Widget filterButton(){
-    return GestureDetector(
-      onTap: (){}, //go to filter page
-      child: Container(
-        padding: EdgeInsets.all(10),
-        margin: EdgeInsets.fromLTRB(0, 5, 10, 0),
-        decoration: BoxDecoration(
-          shape: BoxShape.circle,
-          color: locator<CometThemeManager>().theme.secondaryMono,
-          border: Border.all(
-            color: locator<CometThemeManager>().theme.mainMono,
-            width: 2.0
+
+  Row _searchWidget() {
+    return Row(
+      children: <Widget>[
+        // SearchBar<String>(
+        //   onSearch: (s) {},
+        //   onItemFound: (string, inte) {},
+        // ),
+        Spacer(),
+        GestureDetector(
+          onTap: (){}, //go to filter page
+          child: Container(
+            padding: EdgeInsets.all(10),
+            margin: EdgeInsets.only(right: 10),
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color: locator<CometThemeManager>().theme.secondaryMono,
+              border: Border.all(
+                color: locator<CometThemeManager>().theme.mainMono,
+                width: 2.0
+              )
+            ),
+            child: Icon(Icons.filter_list, color: locator<CometThemeManager>().theme.mainColor, size: 30 )
           )
-        ),
-        child: Icon(Icons.filter_list, color: locator<CometThemeManager>().theme.mainColor, size: 30 )
-      )
+        )
+      ],
     );
   }
 
-  Widget eventCarousel(){
+  Widget _eventCarousel(HomeModel model){
     return Container(
-      margin: EdgeInsets.only(bottom: 30),
       child: CarouselSlider(
-        items: events.map((i) {
+        items: model.events.map((i) {
           return Builder(
             builder: (BuildContext context) {
-              return Padding(
+              return Container(
                 child: i,
-                padding: EdgeInsets.symmetric(horizontal: 3),
               );
             },
           );
         }).toList(),
         options: CarouselOptions(
-          height: MediaQuery.of(context).size.height*0.13,
-          autoPlay: false
-        ),      
+          height: 90*1.2,
+          viewportFraction: 0.84,
+          initialPage: 0,
+          enableInfiniteScroll: true,
+          reverse: false,
+          autoPlay: false,
+          enlargeCenterPage: true,
+          onPageChanged: (number, reason) {},
+          scrollDirection: Axis.horizontal,
+        )  
       ),
     );
   }
@@ -251,8 +254,8 @@ class MapState extends State<Map> {
   }
 
   static final CameraPosition _kHome = CameraPosition(
-    target: LatLng(29.722151, -95.389622),
-    //target: LatLng(-50.606805, 165.972134),
+    //target: LatLng(29.722151, -95.389622),
+    target: LatLng(-50.606805, 165.972134),
     zoom: 14.4746,
   );
 
@@ -271,18 +274,19 @@ class MapState extends State<Map> {
               },
             ),
             Positioned(
-              top: MediaQuery.of(context).size.height * 0.12 + 5,
+              top: 100,
               left: MediaQuery.of(context).size.width * 0.05,
-              child: SearchMapPlaceWidget(
-                darkMode: true,
-                apiKey: Theme.of(context).platform == TargetPlatform.iOS ? "AIzaSyDgldMROs1VzrXoEiCfurKutmOps1sJR-8" : "AIzaSyAeD2KtPAnoJJXvINv6ZYUzLvmZTff406M",
-                onSelected: (place) async {
-                  final geolocation = await place.geolocation;
-                  final GoogleMapController controller = await _controller.future;
-                  controller.animateCamera(CameraUpdate.newLatLng(geolocation.coordinates));
-                  controller.animateCamera(CameraUpdate.newLatLngBounds(geolocation.bounds, 0));
-                },
-              ),
+              child: Container()
+              // child: SearchMapPlaceWidget(
+              //   darkMode: true,
+              //   apiKey: Theme.of(context).platform == TargetPlatform.iOS ? "AIzaSyDgldMROs1VzrXoEiCfurKutmOps1sJR-8" : "AIzaSyAeD2KtPAnoJJXvINv6ZYUzLvmZTff406M",
+              //   onSelected: (place) async {
+              //     final geolocation = await place.geolocation;
+              //     final GoogleMapController controller = await _controller.future;
+              //     controller.animateCamera(CameraUpdate.newLatLng(geolocation.coordinates));
+              //     controller.animateCamera(CameraUpdate.newLatLngBounds(geolocation.bounds, 0));
+              //   },
+              // ),
             ),
           ],
         )
